62eff6818dc53cff2f065f361547c5d00463e310
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-01-30 21:03:03 -0500

    updated design document with notes.

--- ipynb-mode.el
+++ ipynb-mode.el
@@ -1,26 +1,20 @@
+;; this file is not part of emacs
+
 (defvar ipynb-mode-hook nil)
 
+;; keybinding
 (defvar ipynb-mode-map
   "Keymap for ipynb major mode"
   (let ((map (make-keymap)))
     (define-key map "C-c C-c" 'show-raw-ipynb)
     map))
 
+;; autoload
 (add-to-list 'auto-mode-alist '("\\.ipynb\\'" . ipynb-mode))
 
-;; parse a json for "markdown" cells and "code" cell_type.
-;; ignore execution counts
-;; maybe use metadata as tags
-;; outputs
-;; source -- pass to org mode representation
-
-;; put inline displays (html tables, images, widgets, etc) in a dummy result block...
-
-;; images will be a placeholder. real images will be displayed by org machinery when cell is run natively
-;; widgets may or may not be handled by emacs jupyter
-;; tables should be rendered as org tables... maybe if they're really big they should be truncated after a few rows and user should be warned
-
+;; parse ipynb json
 
 ;; font-locking
 
+;; provide
 (provide 'ipynb-mode)
